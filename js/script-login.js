c93f90fe93135cb2956b0e0cc95332fa2d126e6f
rrsfira <163364264+rrsfira@users.noreply.github.com>
2024-05-01 22:47:45 +0700

    Update script-login.js

--- js/script-login.js
+++ js/script-login.js
@@ -9,3 +9,20 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+const form = document.getElementById("registration-form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const selectBox = document.getElementById("account-type");
+    const selectedValue = selectBox.value;
+
+    if (selectedValue === "pembeli") {
+        window.location.href = "pembeli/index.html";
+    } else if (selectedValue === "penjual") {
+        window.location.href = "penjual/index.html";
+    } else if (selectedValue === "admin") {
+        window.location.href = "admin/index.html";
+    }
+});
